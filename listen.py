62b40d5d36227dbd2267d005e8765eaa3f6ec03c
waleed tarazi <waleedaltarazy@gmail.com>
2023-05-31 09:35:21 +0300

    try to revert

--- listen.py
+++ listen.py
@@ -14,3 +14,5 @@ if response.status_code == 200:
     print(f"predicted sentmen for this sentance {text} is: {label}, with score of {score}")
 else:
     print(f"Error: {response.status_code} - {response.text}")
+    
+# this is the fist commit that i will UNDO later on
