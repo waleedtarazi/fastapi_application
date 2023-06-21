9bc3a6a875b2e0adfde0a75a7765ccbc9b553fcf
waleed tarazi <waleedaltarazy@gmail.com>
2023-06-21 18:04:39 +0300

    refactor/UserRoutes.py

--- routes/UserRoute.py
+++ routes/UserRoute.py
@@ -17,58 +17,58 @@ def index():
     raise HTTPException(status_code=404, detail= "no root for this directory for now, please visit us again")
 
 
-@UserRouter.post("/signup" , tags=['User'])
+@UserRouter.post("/signup", tags=['User'])
 async def sign_up(user: UserCreate, db: Session = Depends(get_db_connection)):
     if user.password != user.confirm_password:
-        raise HTTPException(status_code = 400, detail= 'Passwords are not matched, please enter matched passwords')
-    db_user = get_user_by_email(db=db, email=user.email)
+        raise HTTPException(status_code=400, detail='Passwords do not match. Please enter matching passwords.')
+    
+    db_user = get_user_by_email(db, user.email)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    created = create_user(db=db, user=user)
-    return {'access_token': signJWT(created.id)['access_token'],
-             'user': created}
+        raise HTTPException(status_code=400, detail='Email already registered.')
+    
+    created_user = create_user(db, user)
+    access_token = signJWT(created_user.id)['access_token']
+    return {'access_token': access_token, 'user': created_user}
+
 
 # ------------------- LOGIN -------------
 @UserRouter.post('/signin', tags=['User'])
-async def login(user: UserLogIn, db: Session= Depends(get_db_connection)):
-    db_user = get_user_by_email(db, email=user.email)
-    if db_user:
-        if verify_password(user.password,db_user.hashed_password):
-            return {
-                'access_token': signJWT(db_user.id)['access_token'] ,
-                'user': db_user
-            }
-        else:
-            raise HTTPException(status_code=400, detail='Invalid password')
-    else:
-        raise HTTPException(status_code=400, detail='Invalid password')
+async def login(user: UserLogIn, db: Session = Depends(get_db_connection)):
+    db_user = get_user_by_email(db, user.email)
+    if db_user is None or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail='Invalid email or password.')
+    
+    access_token = signJWT(db_user.id)['access_token']
+    return {'access_token': access_token, 'user': db_user}
     
 
 # ------------ GET USER PROFILE -------
-@UserRouter.get('/profile',response_model=User, tags=['User'])
-async def get_profile(user_token: Union[str, None] = Header(None),db: Session = Depends(get_db_connection)):
-    if user_token:
-        db_user = get_user(db= db, user_id=get_JWT_ID(user_token))
-        if db_user is None:
-            raise HTTPException(status_code=404, detail="User not found")
-        return db_user
-    else:
-        raise HTTPException(status_code=401, detail='No token provided!, please LogIn first')
+@UserRouter.get('/profile', response_model=User, tags=['User'])
+async def get_profile(user_token: str = Header(None), db: Session = Depends(get_db_connection)):
+    if not user_token:
+        raise HTTPException(status_code=401, detail='No token provided. Please log in first.')
+    
+    db_user = get_user(db, get_JWT_ID(user_token))
+    if not db_user:
+        raise HTTPException(status_code=404, detail='User not found.')
+    
+    return db_user
     
     
     
 # ---------------- EDIT USER PROFILE -----------
-@UserRouter.put('/profile',response_model= User, tags= ['User'])
-async def edit_profile(user_update:UserUpdate,user_token: Union[str,None] = Header(None), db: Session = Depends(get_db_connection)):
-    if user_token:
-        print(get_JWT_ID(user_token))
-        db_user = get_user(db=db,user_id=get_JWT_ID(user_token))
-        if db_user:
-            return update_user(db = db,user_id= get_JWT_ID(user_token),user_update= user_update)
-        else:
-            raise HTTPException(status_code=404, detail='No user found' )
-    else:
-        raise HTTPException(status_code=401, detail="Not Authorized")
+@UserRouter.put('/profile', response_model=User, tags=['User'])
+async def edit_profile(user_update: UserUpdate, user_token: str = Header(None), db: Session = Depends(get_db_connection)):
+    if not user_token:
+        raise HTTPException(status_code=401, detail='Not authorized. Please log in first.')
+    
+    user_id = get_JWT_ID(user_token)
+    db_user = get_user(db=db, user_id=user_id)
+    
+    if not db_user:
+        raise HTTPException(status_code=404, detail='User not found.')
+    
+    return update_user(db=db, user_id=user_id, user_update=user_update)
         
 
 
@@ -87,5 +87,3 @@ async def get_feelings(month:Union[int, None] = None,
         return db_feelings
     else:
         raise HTTPException(status_code=401, detail="Not Authorized")
-
-
